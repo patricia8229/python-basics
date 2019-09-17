4aa5b72907eb31dcf1d98d0a470158665611b358
trishiaq <patricia.kibunja@gmail.com>
2019-09-17 20:24:13 +0300

    function session

---
+++ function.py
@@ -0,0 +1,51 @@
+#a function is named, reusable block of codes used to perfom a related action
+a=0
+b=0
+a=a+2
+b=b+3
+
+a = 0
+b = []
+c = {}
+d = ()
+#'def' is the keyword python uses to create a new function
+#"add_two_numbers" is the name of our function
+#a and b are called parameters of a function
+#c is the return value
+#creating/defining a function
+def add_two_numbers (a,b):
+    c = a+b
+    return c
+
+def add_two_names (first,last):
+    full_name = first + last
+    return full_name
+
+res = add_two_names ("patricia","wanjiku")
+# print (res)
+
+
+
+student1 = [12,23,34,45,65]
+student2 = [22,23,24,25,26]
+student3 = [32,34,45,45,34]
+student4 = [42,24,45,45,43]
+
+
+
+
+def sum_digits(num):
+    #hint:use a for looop
+    #hint : convert sum to string
+    sum_of_digits = None
+    return sum_of_digits
+
+    def sum_digit(num)
+    sum_of_digits: int = 0
+    num = str(sum)
+    for i in num:
+        sum_of_digits = sum_digits + int (i)
+
+        res = sum_digits
+        #calling our function
+
